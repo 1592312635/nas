ab38c9b83b6ba5d82166840ea5c70440916aeec3
minyan.he <hmy990528@gmail.com>
2024-09-18 18:47:36 +0900

    [master]配置文件提交

--- nas-dao/src/main/java/com/minyan/nasdao/NasAcitvityRewardDAO.java
+++ nas-dao/src/main/java/com/minyan/nasdao/NasAcitvityRewardDAO.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface NasAcitvityRewardDAO extends MyBatisBaseDao<AcitvityRewardPO, Long> {
+
 }
---
+++ nas-mapi/src/main/java/com/minyan/nasmapi/NasMapiApplication.java
@@ -0,0 +1,22 @@
+package com.minyan.nasmapi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAsync
+@EnableCaching
+@EnableAutoConfiguration
+@MapperScan(basePackages = "com.minyan.nasdao")
+public class NasMapiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NasMapiApplication.class, args);
+  }
+}
